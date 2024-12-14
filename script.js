8aa056391711087e67c1dd3bd07682b9e82ea4f1
Samsun Kiran <samsunkiran02@gmail.com>
2024-12-14 09:44:54 +0530

    Basic functionality added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let input = document.getElementById('input');
 let clickbtn = document.getElementById('add');
 let result = document.getElementById('todolist') 
+
+let todos = [];
+
+clickbtn.addEventListener('click', () => {
+    todos.push(input.value);
+    addtodo(input.value)
+})
+
+function addtodo(todo) {
+    let para = document.createElement('p');
+    para.innerText = todo;
+    result.appendChild(para)
+}
