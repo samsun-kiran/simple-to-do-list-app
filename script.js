4b0d993e047d3b2cc24ef09455f13d073a28a964
Samsun Kiran <samsunkiran02@gmail.com>
2024-12-15 07:05:02 +0530

    remove function added

--- script.js
+++ script.js
@@ -14,4 +14,18 @@ function addtodo(todo) {
     let para = document.createElement('p');
     para.innerText = todo;
     result.appendChild(para)
+    para.addEventListener('click', () => {
+        para.style.textDecoration = 'line-through';
+    })
+    para.addEventListener("dblclick", () => {
+        result.removeChild(para);
+        remove(todo);
+    })
+}
+
+function remove(todo) {
+    let index = todos.indexOf(todo);
+    if (index > -1) {
+        todos.slice(index, 1)
+    }
 }
