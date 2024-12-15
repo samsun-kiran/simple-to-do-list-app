0e653b7999d9cea299a6a9a1ae2c8f64d699d05e
Samsun Kiran <samsunkiran02@gmail.com>
2024-12-15 18:10:23 +0530

    Commands added

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
+// Get Elements
 let input = document.getElementById('input');
 let clickbtn = document.getElementById('add');
 let result = document.getElementById('todolist') 
 
+// Array initialization
 let todos = [];
 
+// Windows onload function
 window.onload = () => {
     todos = JSON.parse(localStorage.getItem('todos')) || []
     todos.forEach(todo => addtodo(todo));
 }
 
-
+// Click to push into a Array
 clickbtn.addEventListener('click', () => {
     todos.push(input.value);
-    localStorage.setItem('todos', JSON.stringify(todos))
+    localStorage.setItem('todos', JSON.stringify(todos)) // add to LocalStorage
     addtodo(input.value)
     input.value = '';
 })
 
+// Add to do list function
 function addtodo(todo) {
     let para = document.createElement('p');
     para.innerText = todo;
@@ -26,15 +30,16 @@ function addtodo(todo) {
     })
     para.addEventListener("dblclick", () => {
         result.removeChild(para);
-        remove(todo);
+        remove(todo); // remove function called
     })
 }
 
+// Remove function
 function remove(todo) {
     let index = todos.indexOf(todo);
     if (index > -1) {
-        todos.splice(index, 1)
+        todos.splice(index, 1) // removes the todo
     }
-    localStorage.clear();
-    localStorage.setItem('todos', JSON.stringify(todos))
+    localStorage.clear(); // Clear the Local Storage
+    localStorage.setItem('todos', JSON.stringify(todos)) // Update the new Array to LocalStorage
 }
